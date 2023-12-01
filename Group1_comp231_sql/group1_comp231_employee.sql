d8d8a45caa5babeb884323ebecd0fc3cbf6eb671
you1424 <151393247+you1424@users.noreply.github.com>
2023-12-01 18:31:15 -0500

    Add files via upload
    I add User14 for create IT administrator user

--- Group1_comp231_sql/group1_comp231_employee.sql
+++ Group1_comp231_sql/group1_comp231_employee.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `group1_comp231` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `group1_comp231`;
 -- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
 -- Host: localhost    Database: group1_comp231
@@ -47,7 +45,7 @@ CREATE TABLE `employee` (
 
 LOCK TABLES `employee` WRITE;
 /*!40000 ALTER TABLE `employee` DISABLE KEYS */;
-INSERT INTO `employee` VALUES (1,'Test1','Ftest1','Ltest1',1,1,'Test1',1.0),(2,'Test2','Ftest2','Ltest2',2,2,'Test2',1.5),(3,'Test3','Ftest3','Ltest3',3,3,'Test3',2.0),(4,'Test4','Ftest4','Ltest4',1,4,'Test4',2.5),(5,'Test5','Ftest5','Ltest5',2,1,'Test5',3.0),(6,'Test6','Ftest6','Ltest6',3,2,'Test6',3.5),(7,'Test7','Ftest7','Ltest7',1,3,'Test7',4.0),(8,'Test8','Ftest8','Ltest8',2,4,'Test8',4.5),(9,'Test9','Ftest9','Ltest9',3,1,'Test9',5.0),(10,'Test10','Ftest10','Ltest10',1,2,'Test10',5.5),(11,'Test11','Ftest11','Ltest11',2,3,'Test11',6.0),(12,'Test12','Ftest12','Ltest12',3,4,'Test12',6.5);
+INSERT INTO `employee` VALUES (1,'Test1','Ftest1','Ltest1',1,1,'Test1',1.0),(2,'Test2','Ftest2','Ltest2',2,2,'Test2',1.5),(3,'Test3','Ftest3','Ltest3',3,3,'Test3',2.0),(4,'Test4','Ftest4','Ltest4',1,4,'Test4',2.5),(5,'Test5','Ftest5','Ltest5',2,1,'Test5',3.0),(6,'Test6','Ftest6','Ltest6',3,2,'Test6',3.5),(7,'Test7','Ftest7','Ltest7',1,3,'Test7',4.0),(8,'Test8','Ftest8','Ltest8',2,4,'Test8',4.5),(9,'Test9','Ftest9','Ltest9',3,1,'Test9',5.0),(10,'Test10','Ftest10','Ltest10',1,2,'Test10',5.5),(11,'Test11','Ftest11','Ltest11',2,3,'Test11',6.0),(12,'Test12','Ftest12','Ltest12',3,4,'Test12',6.5),(13,'Test13','Ftest13','Ltest13',4,3,'Test13',7.0);
 /*!40000 ALTER TABLE `employee` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -60,4 +58,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-11-18 12:33:08
+-- Dump completed on 2023-12-01 18:28:15
